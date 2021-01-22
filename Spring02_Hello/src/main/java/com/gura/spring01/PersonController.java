c10407fa2170f18e985fb0119f46ebe5b80a54a9
likuimin04 <likuimin04@outlook.com>
2021-01-22 12:31:11 +0900

    21.01.22 오후 12:31

--- Spring02_Hello/src/main/java/com/gura/spring01/PersonController.java
+++ Spring02_Hello/src/main/java/com/gura/spring01/PersonController.java
@@ -5,12 +5,15 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/* component scan 될때 PersonController 객체가 생성이 되고 관리가 된다. */
 @Controller
 public class PersonController {
 	@RequestMapping("/person.do")
 	public String person(HttpServletRequest request) {
-		String personToday="우리 쌤";
+		String personToday="우리 쌤"; //data type=model 이라고도 불린다.
 		request.setAttribute("personToday", personToday);
-		return "person";
+		
+		// forward 이동할 view page 의 위치를 리턴해준다.
+		return "person"; // /WEB-INF/view/persom.jsp
 	}
 }
