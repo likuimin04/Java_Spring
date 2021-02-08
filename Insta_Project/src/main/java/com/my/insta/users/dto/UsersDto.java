9c1250b7d60c967f64fc6b84cdec8cda1dadd457
likuimin04 <likuimin04@outlook.com>
2021-02-08 15:47:50 +0900

    21.02.08 오후15:47 추가함

---
+++ Insta_Project/src/main/java/com/my/insta/users/dto/UsersDto.java
@@ -0,0 +1,112 @@
+package com.my.insta.users.dto;
+
+public class UsersDto {
+	private String id;
+	private String pwd;
+	private String email;
+	private String profile;
+	private String regdate;
+	private String name;
+	private String tel;
+	private String gender;
+	private String addr;
+	private String newPwd;
+	
+	//디폴트 생성자
+	public UsersDto() {}
+
+	public UsersDto(String id, String pwd, String email, String profile, String regdate, String name, String tel,
+			String gender, String addr, String newPwd) {
+		super();
+		this.id = id;
+		this.pwd = pwd;
+		this.email = email;
+		this.profile = profile;
+		this.regdate = regdate;
+		this.name = name;
+		this.tel = tel;
+		this.gender = gender;
+		this.addr = addr;
+		this.newPwd = newPwd;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+
+	public String getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getNewPwd() {
+		return newPwd;
+	}
+
+	public void setNewPwd(String newPwd) {
+		this.newPwd = newPwd;
+	}
+}
